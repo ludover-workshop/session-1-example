0e989e998b94631b932de3f9cd2ffdb3147e5f4c
pablitar <pablitar@gmail.com>
2021-05-13 22:42:25 -0300

    Agregados algunos efectos y feedback

--- behaviours/projectile.gd
+++ behaviours/projectile.gd
@@ -4,6 +4,8 @@ class_name Projectile
 export(float) var speed = 900
 export(float) var time_to_live = 3
 
+var removed = false
+
 func _physics_process(delta):
 	var velocity = Vector2(speed, 0).rotated(self.rotation)
 	
@@ -11,12 +13,17 @@ func _physics_process(delta):
 	
 	time_to_live -= delta
 	
-	if time_to_live <= 0:
-		queue_free()
+	if time_to_live <= 0 && !removed:
+		removed = true
+		remove()
 
 
 func _on_Area2D_body_entered(body):
 	on_hit(body)
+	remove()
+
+
+func remove():
 	queue_free()
 
 func on_hit(body):
--- fireball/Fireball.gd
+++ fireball/Fireball.gd
@@ -3,5 +3,17 @@ extends Projectile
 export(float) var damage = 10
 export(float) var knockback_speed = 70
 
+
 func on_hit(body):
 	DamageableBehaviour.do_damage(body, damage, self.position, knockback_speed)
+
+func remove():
+	$Area2D.queue_free()
+	$AnimatedSprite.queue_free()
+		
+	$Particles2D.emitting = false
+	
+	yield(get_tree().create_timer($Particles2D.lifetime), "timeout")
+		
+	queue_free()
+	
